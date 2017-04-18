1331f69101fd9315a954db3039d335f17484d6b6
Ansem911 <ansem991@gmail.com>
2017-04-17 20:58:58 -0500

    Added comments for parser

--- src/Driver.java
+++ src/Driver.java
@@ -72,19 +72,13 @@ public class Driver {
         return retVal;
     }
 
+	/**
+     * Parses args from the command line and the input file.
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
-        //Variable queryVar = new Variable('B');
-        //ArrayList<Variable> evidence = new ArrayList<>();
-        //evidence.add(new Variable('J', true));
-        //evidence.add(new Variable('M', true));
         BayesNet bayesNet = new BayesNet();
-        /*bayesNet.add('B', 0.001);
-        bayesNet.add('E', 0.002);
-        bayesNet.add(new char[]{'B', 'E'}, 'A', new double[]{0.001, 0.29, 0.94, 0.95});
-        bayesNet.add(new char[]{'A'}, 'J', new double[]{0.05, 0.9});
-        bayesNet.add(new char[]{'A'}, 'M', new double[]{0.01, 0.7});*/
 		
-		//DEBUG remember to change 0 to 3
 		if (args.length != 3) {
             System.out.println("Incorrect number of arguments.");
             System.exit(1);
@@ -94,23 +88,18 @@ public class Driver {
 			String mechanism = args[1];
 			String query = args[2];
 			
-			//DEBUG Remember to uncomment above
-			/*Scanner in = new Scanner(new File("input.txt"));
-			String mechanism = "elim";
-			String query = "P(B|J=t,M=t)";*/
-			
 			//Parser for query
-			Pattern queryvariable = Pattern.compile("P\\([A-Z]");
-			Pattern queryevidence = Pattern.compile("[A-Z]\\=[t,f]");
+		Pattern queryvariable = Pattern.compile("P\\([A-Z]"); //Matches to find the query variable
+			Pattern queryevidence = Pattern.compile("[A-Z]=[t,f]"); //Matches to find the evidence variables and their values
 			Matcher qv = queryvariable.matcher(query);
 			Matcher qe = queryevidence.matcher(query);
 			qv.find();
-			Variable queryVar = new Variable(qv.group().charAt(2));
-			ArrayList<Variable> evidence = new ArrayList<>();
+			Variable queryVar = new Variable(qv.group().charAt(2)); //Variable to be queried
+			ArrayList<Variable> evidence = new ArrayList<>(); //List containing the evidence of the query
 			while(qe.find())
 			{
 				String temp = qe.group();
-				if(temp.charAt(2)=='t')
+				if(temp.charAt(2)=='t') //True if evidence is true and to the evidence
 				{
 					evidence.add(new Variable(temp.charAt(0),true));
 				}
@@ -121,42 +110,46 @@ public class Driver {
 			}
 			
 			// Parser for input file
-			Pattern single = Pattern.compile("P\\([A-Z]\\)");
-			Pattern multi = Pattern.compile("([A-Z]\\s)+\\|\\s[A-Z]");
-			Pattern truefalse = Pattern.compile("([t,f]\\s)+");
-			Pattern probability = Pattern.compile("\\d*\\.\\d+");
-			while(in.hasNextLine())
+			Pattern single = Pattern.compile("P\\([A-Z]\\)"); //Matches to find the variable of a node with no parents
+			Pattern multi = Pattern.compile("([A-Z]\\s)+\\|\\s[A-Z]"); //Matches to find the variables of a node with parents
+			Pattern truefalse = Pattern.compile("([t,f]\\s)+"); //Matches to determine the values of the ancestor nodes for a probability
+			Pattern probability = Pattern.compile("\\d*\\.\\d+"); //Matches the probability
+			while(in.hasNextLine()) //Runs until the end of the file
 			{
 				String givenLine = in.nextLine();
 				Matcher s = single.matcher(givenLine);
 				Matcher m = multi.matcher(givenLine);
-				if(s.find())
+				if(s.find()) //True if the line is for a node with no parents
 				{
 					Matcher p = probability.matcher(givenLine);
 					p.find();
-					bayesNet.add(s.group().charAt(2),Double.parseDouble(p.group()));
+					bayesNet.add(s.group().charAt(2),Double.parseDouble(p.group())); //Adds the variable to the bayes net with its probability
 					in.nextLine();
 				}
-				else if(m.find())
+				else if(m.find()) //True if the line is for a node with parents
 				{
 					String variables = m.group().replaceAll(" ","").replaceAll("\\|","");
 					in.nextLine();
-					int totalvariables = variables.length()-1;
-					char[] leftVariables = variables.substring(0,totalvariables).toCharArray();
-					char rightVariable = variables.charAt(totalvariables);
-					int totalCombinations = (int)Math.pow(2,totalvariables);
-					double[] probabilitiesOfNode = new double[totalCombinations];
-					for(int x = 0; x < totalCombinations; x++)
+					int totalvariables = variables.length()-1; //Determines the total variables of the table (parents plus current node)
+					char[] leftVariables = variables.substring(0,totalvariables).toCharArray(); //Gets the variables which are the parents of the node
+					char rightVariable = variables.charAt(totalvariables); //Gets the variable of the node
+					int totalCombinations = (int)Math.pow(2,totalvariables); //Determines the number of combinations that are needed (2^parents)
+					double[] probabilitiesOfNode = new double[totalCombinations]; //Array containing the probabilities of a node
+					for(int x = 0; x < totalCombinations; x++) //Retrieves and sets all of the probabilities of a given node
 					{
 						givenLine = in.nextLine();
 						Matcher p = probability.matcher(givenLine);
 						Matcher tf = truefalse.matcher(givenLine);
 						p.find();
 						tf.find();
+						/*
+						 * The probability is placed into the array using a binary value which is calculated from the true and false values
+						 * For example, a node with two parents will have a probability where both of those parents are true
+						 * which maps to the binary value 11. That integer value is 3 so the probability gets put into the array's index of 3.
+						 */
 						probabilitiesOfNode[Integer.parseInt(tf.group().replaceAll("t ","1").replaceAll("f ","0"),2)] = Double.parseDouble(p.group());
 					}
-					System.out.printf("%s %s\n%s",Arrays.toString(leftVariables),rightVariable,Arrays.toString(probabilitiesOfNode)); //DEBUG
-					bayesNet.add(leftVariables,rightVariable,probabilitiesOfNode);
+					bayesNet.add(leftVariables,rightVariable,probabilitiesOfNode); //Adds the variable to the bayes net with its probabilities and corresponding parent values
 				}
 				
 			}
