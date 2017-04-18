4cb310aa64ff870e8ed4e4fb97222a4d79e8af97
Terrence Park <trex@parkvue.com>
2017-04-17 20:33:41 -0500

    Fixed up print formatting, added comments

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.TreeSet;
 
 public class Driver {
 
-    public void enumerationAsk(Variable queryVar, ArrayList<Variable> evidence, BayesNet bayesNet) {
+    public void enumerationAsk(Variable queryVar, TreeSet<Variable> evidence, BayesNet bayesNet) {
         ArrayList<Variable> vars = bayesNet.getVars(evidence);
         evidence.add(queryVar);
 
@@ -24,8 +26,7 @@ public class Driver {
         queryVar.setValue(true);
         double trueProbability = enumerateAll(vars, evidence, bayesNet);
 
-//        evidence.remove(evidence.size()-1);
-//        double evidenceProbability = enumerateAll(vars, evidence, bayesNet);
+        evidence.remove(queryVar);
 
         System.out.println();
         System.out.println("RESULT:");
@@ -37,7 +38,7 @@ public class Driver {
         System.out.printf("P(%s | %s) = %.16f%n", queryVar, evidence.toString().replaceAll("[\\[\\]]", ""), trueProbability/(falseProbability+trueProbability));
     }
 
-    public double enumerateAll(ArrayList<Variable> vars, ArrayList<Variable> evidence, BayesNet bayesNet) {
+    public double enumerateAll(ArrayList<Variable> vars, TreeSet<Variable> evidence, BayesNet bayesNet) {
         if (vars.isEmpty()) {
             return 1;
         }
@@ -51,33 +52,74 @@ public class Driver {
             double probability = bayesNet.getProbability(first, evidence);
             evidence.add(first);
             retVal += probability * enumerateAll(vars, evidence, bayesNet);
-            evidence.remove(evidence.size()-1);
+            evidence.remove(first);
 
             first.setValue(true);
             probability = bayesNet.getProbability(first, evidence);
             evidence.add(first);
             retVal += probability * enumerateAll(vars, evidence, bayesNet);
-            evidence.remove(evidence.size()-1);
+            evidence.remove(first);
 
             first.unsetValue();
         }
         vars.add(0, first);
-        System.out.printf("%s    %s    %.8f%n", vars, evidence, retVal);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < bayesNet.size(); i++) {
+            if (i < vars.size()) {
+                sb.append(vars.get(i).getName()).append(' ');
+            } else {
+                sb.append("  ");
+            }
+        }
+        sb.append("      | ");
+        Iterator<Variable> it = evidence.iterator();
+        for (int i = 0; i < bayesNet.size(); i++) {
+            if (it.hasNext()) {
+                Variable v = it.next();
+                sb.append(v.getName()).append('=').append(v.getValue() ? 't' : 'f').append(' ');
+            } else {
+                sb.append("    ");
+            }
+        }
+        sb.append("      = ").append(String.format("%.8f", retVal));
+        System.out.println(sb);
 
         return retVal;
     }
 
     public static void main(String[] args) {
-        Variable queryVar = new Variable('B');
-        ArrayList<Variable> evidence = new ArrayList<>();
-        evidence.add(new Variable('J', true));
-        evidence.add(new Variable('M', true));
+        //alarm.bn
+//        Variable queryVar = new Variable('E');
+//        TreeSet<Variable> evidence = new TreeSet<>();
+//        evidence.add(new Variable('J', true));
+//        evidence.add(new Variable('M', true));
+//        BayesNet bayesNet = new BayesNet();
+//        bayesNet.add('B', 0.001);
+//        bayesNet.add('E', 0.002);
+//        bayesNet.add(new char[]{'B', 'E'}, 'A', new double[]{0.001, 0.29, 0.94, 0.95});
+//        bayesNet.add(new char[]{'A'}, 'J', new double[]{0.05, 0.9});
+//        bayesNet.add(new char[]{'A'}, 'M', new double[]{0.01, 0.7});
+
+        //Some random test
+//        Variable queryVar = new Variable('A');
+//        TreeSet<Variable> evidence = new TreeSet<>();
+//        evidence.add(new Variable('B', true));
+//        BayesNet bayesNet = new BayesNet();
+//        bayesNet.add('A', 0.001);
+//        bayesNet.add(new char[]{'A'}, 'B', new double[]{0.05, 0.9});
+
+        //ex2.bn
+        Variable queryVar = new Variable('A');
+        TreeSet<Variable> evidence = new TreeSet<>();
+        evidence.add(new Variable('D', true));
+        evidence.add(new Variable('E', true));
         BayesNet bayesNet = new BayesNet();
-        bayesNet.add('B', 0.001);
-        bayesNet.add('E', 0.002);
-        bayesNet.add(new char[]{'B', 'E'}, 'A', new double[]{0.001, 0.29, 0.94, 0.95});
-        bayesNet.add(new char[]{'A'}, 'J', new double[]{0.05, 0.9});
-        bayesNet.add(new char[]{'A'}, 'M', new double[]{0.01, 0.7});
+        bayesNet.add('A', 0.3);
+        bayesNet.add('B', 0.6);
+        bayesNet.add(new char[]{'A'}, 'C', new double[]{0.4, 0.8});
+        bayesNet.add(new char[]{'A', 'B'}, 'D', new double[]{0.2, 0.1, 0.8, 0.7});
+        bayesNet.add(new char[]{'C'}, 'E', new double[]{0.2, 0.7});
 
         new Driver().enumerationAsk(queryVar, evidence, bayesNet);
     }
--- src/Variable.java
+++ src/Variable.java
@@ -1,36 +1,67 @@
-public class Variable {
+/**
+ * Object to represent a variable and its assignment.
+ */
+public class Variable implements Comparable<Variable> {
 
-    private char name;
-    private boolean isSet;
-    private boolean value;
+    private char name;  // name of the variable
+    private boolean isSet;  // true if the variable has been set
+    private boolean value;  // value of the variable
 
+    /**
+     * Create unset variable.
+     * @param name variable name
+     */
     public Variable(char name) {
         this.name = name;
     }
 
+    /**
+     * Create preset variable.
+     * @param name variable name
+     * @param value preset variable value
+     */
     public Variable(char name, boolean value) {
         this(name);
         isSet = true;
         this.value = value;
     }
 
+    /**
+     * Gets the name of the variable.
+     * @return variable name
+     */
     public char getName() {
         return name;
     }
 
+    /**
+     * Gets the value of the variable.
+     * @return variable value if it has been set, false otherwise
+     */
     public boolean getValue() {
-        return value;
+        return isSet ? value : false;
     }
 
+    /**
+     * Sets the value of the variable.
+     * @param val value to set the variable to
+     */
     public void setValue(boolean val) {
         value = val;
         isSet = true;
     }
 
+    /**
+     * Clears the value of the variable.
+     */
     public void unsetValue() {
         isSet = false;
     }
 
+    /**
+     * Gets if the variable is set.
+     * @return true if variable has been set
+     */
     public boolean isSet() {
         return isSet;
     }
@@ -52,5 +83,11 @@ public class Variable {
     @Override
     public String toString() {
         return String.format("%c%s", name, isSet ? String.format(" = %c", value ? 't' : 'f') : "");
+    }
+
+    @Override
+    // Alphabetical order
+    public int compareTo(Variable other) {
+        return name - other.name;
     }
 }
